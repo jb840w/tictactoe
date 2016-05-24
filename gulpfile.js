eee16c967e2bc7165a7413ffec42b14bb42b0d61
Jeremy Benson <jeremy@ashleybensonfitness.com>
2016-05-24 13:12:37 -0500

    added more structure

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+// Gulp dependencies go here
+
+gulp.task('default', function() {
+  // Gulp tasks go here
+
+  // Run eslint
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
+  // Node source
+  gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+  // Browser source
+  gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
+});
